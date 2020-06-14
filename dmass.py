6f451ebbd60e5a61e3594339d424f2072b5bc59b
avi9007 <66898303+avi9007@users.noreply.github.com>
2020-06-14 12:51:06 +0600

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -44,4 +44,4 @@ async def send(ctx, *, content: str):
                 await client.say("DM can't Sent To : {} :x: ".format(member))
 
 
-client.run("NjA4MDcxNTEyMTg3OTk0MTQy.XXHE4w.7x4jawjHJYWctmaWbBa_5pMN484")                
+client.run("NzIxNjA5Njk0NDkzNTQwNDAy.XuXIwA.gCk1pLaSoL0xyE0sHX7C07hQURM")                
